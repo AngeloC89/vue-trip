69c59705f4918c3e8c00888d12920dce76e9029d
AngeloC89 <Angelociulla89@gmail.com>
2024-08-09 19:40:46 +0200

    make base layout

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="text-center m-3 d-flex flex-column  justify-content-center align-items-center ">
+            <h1>Posti Da Vedere</h1>
+            <select class="form-select w-25" aria-label="Default select example"  v-model="done"
+                name="done">
+                <option value="">Tutti</option>
+                <option value="true">Visti</option>
+                <option value="false">Da vedere</option>
+            </select>
+        </div>
+    </header>
+</template>
+
+<script>
+
+    export default {
+        name: 'HeaderComponent',
+        data() {
+            return {
+            
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <main class="my-4">
+        <div class="container">
+            <div class="row gap-1">
+                <div class="col-sm-4 debug">
+                    card for day
+                </div>
+                <div class="col-sm-4 debug">
+                    card for day
+                </div>
+                <div class="col-sm-4 debug">
+                    card for day
+                </div>
+                <div class="col-sm-4 debug">
+                    card for day
+                </div>
+                <div class="col-sm-4 debug">
+                    card for day
+                </div>
+
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .debug {
+        border: 1px solid red;
+        height: 100px;
+        width: calc(100% / 3 - 4px);
+
+    }
+
+
+</style>
